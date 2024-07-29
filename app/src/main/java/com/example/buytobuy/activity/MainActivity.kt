a5760d031c28ea23a0aadad1f4713d891d35e611
cihancelik7 <cihan.celik7@gmail.com>
2024-07-29 13:33:52 +0300

    main screen slider screen added

---
+++ app/src/main/java/com/example/buytobuy/activity/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.buytobuy.activity
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import com.example.buytobuy.R
+import com.example.buytobuy.SliderAdapter
+import com.example.buytobuy.databinding.ActivityMainBinding
+import com.example.buytobuy.model.SliderModel
+import com.example.buytobuy.viewModel.MainViewModel
+import com.google.firebase.FirebaseApp
+
+class MainActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel = MainViewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.statusBarColor = resources.getColor(R.color.purple, theme)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+        initBanner()
+
+
+    }
+
+    private fun initBanner() {
+        binding.progressBarBanner.visibility = View.VISIBLE
+        viewModel.banners.observe(this, Observer { items ->
+        banners(items)
+            binding.progressBarBanner.visibility =View.GONE
+        })
+        viewModel.loadBanners()
+    }
+
+    private fun banners(images: List<SliderModel>) {
+        binding.viewpagerSlider.adapter = SliderAdapter(images, binding.viewpagerSlider)
+        binding.viewpagerSlider.clipToPadding = false
+        binding.viewpagerSlider.clipChildren = false
+        binding.viewpagerSlider.offscreenPageLimit=3
+        binding.viewpagerSlider.getChildAt(0).overScrollMode= RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePageTransformer = CompositePageTransformer().apply {
+            addTransformer(MarginPageTransformer(40))
+        }
+        binding.viewpagerSlider.setPageTransformer(compositePageTransformer)
+        if (images.size>1){
+            binding.dotIndicator.visibility=View.VISIBLE
+            binding.dotIndicator.attachTo(binding.viewpagerSlider)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/buytobuy/model/SliderModel.kt
@@ -0,0 +1,3 @@
+package com.example.buytobuy.model
+
+data class SliderModel(val url:String="")
